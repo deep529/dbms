be1f969bd6b6ae9ca90a36257055063dccf232ad
deep529 <ddiwani3008@gmail.com>
2019-03-10 02:53:12 +0530

    Added Airport csv files

--- 2_relation_model/process_data/remove_duplicate_rows.py
+++ 2_relation_model/process_data/remove_duplicate_rows.py
@@ -1,21 +1,24 @@
-""" Removes duplicate lines which are adjacent """
+""" Removes all duplicate lines """
 import sys
 
 def remove_dup_lines(in_file_name, out_file_name):
     in_file = open(in_file_name, 'r')
     out_file = open(out_file_name, 'w')
 
-    prev_line = ""
+    prev_lines = set()
     count = 0
+    duplicates = 0
 
     for line in in_file.readlines():
-        if line != prev_line:
+        if line in prev_lines:
+            duplicates += 1
+        else:
             out_file.write(line)
-            prev_line = line
+            prev_lines.add(line)
             count += 1
 
-
     print("{} rows written".format(count))
+    print("{} duplicates found".format(duplicates))
     in_file.close()
     out_file.close()
 
