4f12805e37f7df2215786a451f911f8538519a7f
Devansh Agarwal <thedevanshagarwal@gmail.com>
2019-04-05 02:12:37 +0530

    Add files via upload

---
+++ 3_advanced_sql_queries/query14.sql
@@ -0,0 +1,1 @@
+select distinct airline_carrier_code,distance,origin_airport_long_id, dest_airport_long_id   from flight where (distance,airline_carrier_code) in (select max(distance), airline_carrier_code from flight group by(airline_carrier_code));
