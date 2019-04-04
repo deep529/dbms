80b4c3afc1c864477cdfe6f94a2cc6593c40d135
Devansh Agarwal <thedevanshagarwal@gmail.com>
2019-04-05 00:57:41 +0530

    Add files via upload

---
+++ 3_advanced_sql_queries/query19.sql
@@ -0,0 +1,1 @@
+select day from(select count (a.day) as num ,a.day as day from (select to_char(make_date(year, month, day_of_month), 'day') as day from flight where cancel_code = 'A' or cancel_code = 'B' or cancel_code = 'C') a group by (a.day)) b order by num desc limit 1; 
