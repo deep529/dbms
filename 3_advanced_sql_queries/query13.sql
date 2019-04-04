15e0b1821ee80bc211050981ff81c7480751ca8b
Devansh Agarwal <thedevanshagarwal@gmail.com>
2019-04-05 02:36:49 +0530

    Add files via upload

---
+++ 3_advanced_sql_queries/query13.sql
@@ -0,0 +1,1 @@
+select avg(num) from (select count(*) as num,month from (select * from flight where arr_time != '') a where  dest_airport_long_id in (select long_term_id from airport where state_name like 'Colorado') and arr_time::INTEGER >= 0300 and arr_time::INTEGER <= 0600 group by month) a;  
