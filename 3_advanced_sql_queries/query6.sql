6dc72d5030b60c178f0fdad7fe4b2d07c6a63cde
Devansh Agarwal <thedevanshagarwal@gmail.com>
2019-04-05 13:06:32 +0530

    Add files via upload

---
+++ 3_advanced_sql_queries/query6.sql
@@ -0,0 +1,1 @@
+select day from(select avg(arr_delay), day from (select to_char(make_date(flight_year, flight_month, flight_day_of_month), 'day') as day,arr_delay from summary ) a group by day ) b order by (avg) desc limit 1 ;
