71978337e2b3b91ebc0ae8fa3a12158d9bd6df6f
Devansh Agarwal <thedevanshagarwal@gmail.com>
2019-04-05 23:40:59 +0530

    Update query18.sql

--- 3_advanced_sql_queries/query18.sql
+++ 3_advanced_sql_queries/query18.sql
@@ -1,1 +1,6 @@
-select flight_number, airline_carrier_code from flight where (origin_airport_long_id,origin_airport_seq_id) in (select distinct long_term_id,sequence_id from airport where airport_code='SFO') and diverted_landings!=0;
+select F.flight_number, A.airline_name
+from flight as F join airline as A
+on (F.airline_carrier_code = A.unique_carrier_code)
+where (F.origin_airport_long_id, F.origin_airport_seq_id)
+in (select distinct long_term_id,sequence_id from airport where airport_code='SFO') 
+and diverted_landings != 0;
