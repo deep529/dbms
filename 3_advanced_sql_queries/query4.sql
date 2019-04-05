161c7ac7279bf3499e786d8ca90203f1f8a2f9e4
Devansh Agarwal <thedevanshagarwal@gmail.com>
2019-04-05 17:45:07 +0530

    Add files via upload

---
+++ 3_advanced_sql_queries/query4.sql
@@ -0,0 +1,1 @@
+select distinct airline_carrier_code from (select airline_carrier_code, to_char(make_date(year, month, day_of_month), 'WW') as week_number from flight) a group by (airline_carrier_code, week_number ) having count(*) < 10000;
