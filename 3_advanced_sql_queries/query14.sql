6df92d5bd98670086c490ae643ee281a13c6d4fd
deep529 <ddiwani3008@gmail.com>
2019-04-05 19:14:48 +0530

    Updated query14 with order by distance

--- 3_advanced_sql_queries/query14.sql
+++ 3_advanced_sql_queries/query14.sql
@@ -1,1 +1,5 @@
-select distinct airline_carrier_code,distance,origin_airport_long_id, dest_airport_long_id   from flight where (distance,airline_carrier_code) in (select max(distance), airline_carrier_code from flight group by(airline_carrier_code));
+select distinct airline_carrier_code, distance, origin_airport_long_id, dest_airport_long_id 
+from flight 
+where (distance,airline_carrier_code) in
+(select max(distance), airline_carrier_code from flight group by(airline_carrier_code)) 
+order by distance asc;
