304fd51419e94d8eac61a2699ceaf123ce268648
Devansh Agarwal <thedevanshagarwal@gmail.com>
2019-04-05 23:45:16 +0530

    Update query17.sql

--- 3_advanced_sql_queries/query17.sql
+++ 3_advanced_sql_queries/query17.sql
@@ -1,5 +1,6 @@
-select  flight_month, flight_airline_carrier_code 
-from diversion 
-group by (flight_month, flight_airline_carrier_code) 
-having count(flight_month) >3 
-order by flight_month ;
+select D.flight_month, A.airline_name 
+from diversion as D join airline as A
+on (D.flight_airline_carrier_code = A.unique_carrier_code)
+group by (D.flight_month, A.airline_name) 
+having count(D.flight_month) >3 
+order by D.flight_month;
