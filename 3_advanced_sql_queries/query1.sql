365b2a2abc8a711d38b3b586a63eaf1c6c30f33c
Deep Diwani <ddiwani3008@gmail.com>
2019-04-05 23:07:16 +0530

    updated query1

--- 3_advanced_sql_queries/query1.sql
+++ 3_advanced_sql_queries/query1.sql
@@ -1,1 +1,6 @@
-select distinct flight_number from flight where origin_airport_long_id in(select long_term_id from airport where city_name like 'New York%' ) and dest_airport_long_id in (select long_term_id from airport where city_name like 'Chicago%') and trim(to_char(make_date(year, month, day_of_month), 'day'))= 'sunday' and airline_carrier_code = 'TSF';
+select distinct flight_number
+from flight
+where origin_airport_long_id in (select long_term_id from airport where city_name like '%New York%' )
+and dest_airport_long_id in (select long_term_id from airport where city_name like '%Chicago%') 
+and trim(to_char(make_date(year, month, day_of_month), 'day'))= 'sunday' 
+and airline_carrier_code = (select unique_carrier_code from airline where airline_name = 'American Central Airlines Inc.');
