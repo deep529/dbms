a1ca33614e68b8ecbebdb4e346b7517cb1a043b5
Devansh Agarwal <thedevanshagarwal@gmail.com>
2019-04-05 17:21:38 +0530

    Add files via upload

---
+++ 3_advanced_sql_queries/query1.sql
@@ -0,0 +1,1 @@
+select distinct flight_number from (select to_char(make_date(year, month, day_of_month), 'day') as day,* from flight) a  where origin_airport_long_id in(select long_term_id from airport where city_name like 'New York%' ) and dest_airport_long_id in (select long_term_id from airport where city_name like 'Chicago%') and trim(day)= 'sunday' and airline_carrier_code = 'TSF';
