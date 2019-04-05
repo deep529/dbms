f0862de72268305faecacbec8fc5d731027d6a00
Deep Diwani <ddiwani3008@gmail.com>
2019-04-05 23:20:42 +0530

    Update query20.sql

--- 3_advanced_sql_queries/query20.sql
+++ 3_advanced_sql_queries/query20.sql
@@ -1,1 +1,6 @@
-select make_date(year, month, day_of_month) as flight_date, airline_carrier_code, origin_airport_long_id from flight where weather_delay > 0;
+select make_date(F.year, F.month, F.day_of_month) as flight_date, 
+A.airline_name,
+F.origin_airport_long_id
+from flight as F join airline as A
+on F.airline_carrier_code = A.unique_carrier_code
+where weather_delay > 0;
