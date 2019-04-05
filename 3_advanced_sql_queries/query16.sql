e79cbd41885516c8c9e84215d53cf3da30aaca45
Devansh Agarwal <thedevanshagarwal@gmail.com>
2019-04-05 23:29:59 +0530

    indent

--- 3_advanced_sql_queries/query16.sql
+++ 3_advanced_sql_queries/query16.sql
@@ -1,1 +1,4 @@
-select flight_month, flight_number from summary where (dep_delay, flight_month) in (select max(dep_delay), flight_month from summary group by(flight_month)) order by flight_month;
+select flight_month, flight_number 
+from summary 
+where (dep_delay, flight_month) in (select max(dep_delay), flight_month from summary group by(flight_month)) 
+order by flight_month;
