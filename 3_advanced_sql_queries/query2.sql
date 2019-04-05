f0f5e175c8527a827de739bdb5a0ee07e4b20c1c
Devansh Agarwal <thedevanshagarwal@gmail.com>
2019-04-05 22:57:07 +0530

    indent

--- 3_advanced_sql_queries/query2.sql
+++ 3_advanced_sql_queries/query2.sql
@@ -1,1 +1,9 @@
-select count(airline_carrier_code), airline_carrier_code from flight where origin_airport_long_id in (select long_term_id from airport where city_name like 'New York%' ) and dest_airport_long_id in (select long_term_id from airport where city_name like 'Chicago%') and day_of_month >= 1 and day_of_month <=7 and month = 1 group by airline_carrier_code order by count(airline_carrier_code) desc;
+select count(airline_carrier_code), airline_carrier_code 
+from flight 
+where origin_airport_long_id in (select long_term_id from airport where city_name like 'New York%' ) 
+and dest_airport_long_id in (select long_term_id from airport where city_name like 'Chicago%') 
+and day_of_month >= 1 
+and day_of_month <=7 
+and month = 1 
+group by airline_carrier_code 
+order by count(airline_carrier_code) desc;
