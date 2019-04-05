9418d811f1065eb0d1512de7a4a64a95374c65dc
Devansh Agarwal <thedevanshagarwal@gmail.com>
2019-04-05 23:02:47 +0530

    indent

--- 3_advanced_sql_queries/query3.sql
+++ 3_advanced_sql_queries/query3.sql
@@ -1,1 +1,5 @@
-select distinct(temp.airline_carrier_code) from (select count((day_of_month, month, airline_carrier_code)) as num_flights , day_of_month, month, airline_carrier_code from flight  group by (day_of_month, month, airline_carrier_code)) temp where num_flights > 1000;
+select distinct(temp.airline_carrier_code) 
+from (select count((day_of_month, month, airline_carrier_code)) as num_flights , day_of_month, month, airline_carrier_code 
+      from flight
+      group by (day_of_month, month, airline_carrier_code)) temp 
+where num_flights > 1000;
