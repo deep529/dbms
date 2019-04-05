bcb31bca360692f1e0e5d16cf53d3ed9edb93b03
Devansh Agarwal <thedevanshagarwal@gmail.com>
2019-04-05 23:05:15 +0530

    indent

--- 3_advanced_sql_queries/query4.sql
+++ 3_advanced_sql_queries/query4.sql
@@ -1,1 +1,4 @@
-select distinct airline_carrier_code from (select airline_carrier_code, to_char(make_date(year, month, day_of_month), 'WW') as week_number from flight) a group by (airline_carrier_code, week_number ) having count(*) < 10000;
+select distinct airline_carrier_code 
+from (select airline_carrier_code, to_char(make_date(year, month, day_of_month), 'WW') as week_number from flight) a 
+group by (airline_carrier_code, week_number ) 
+having count(*) < 10000;
