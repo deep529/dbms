7fd718245151473e41644ee4dc291cb3dad55207
Devansh Agarwal <thedevanshagarwal@gmail.com>
2019-04-05 23:27:49 +0530

    indent

--- 3_advanced_sql_queries/query17.sql
+++ 3_advanced_sql_queries/query17.sql
@@ -1,1 +1,5 @@
-select  flight_month, flight_airline_carrier_code from diversion group by (flight_month, flight_airline_carrier_code) having count(flight_month) >3 order by flight_month ;
+select  flight_month, flight_airline_carrier_code 
+from diversion 
+group by (flight_month, flight_airline_carrier_code) 
+having count(flight_month) >3 
+order by flight_month ;
