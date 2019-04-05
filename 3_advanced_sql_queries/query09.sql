3381ac9d595ca0c18a6abb9cc7fd42c4dedbd5c0
Deep Diwani <ddiwani3008@gmail.com>
2019-04-05 23:34:49 +0530

    Update query09.sql

--- 3_advanced_sql_queries/query09.sql
+++ 3_advanced_sql_queries/query09.sql
@@ -1,11 +1,15 @@
-select distinct origin_airport_long_id
-from flight
-where
-case when 
-	((crs_arr_time::INTEGER)/100)*60 + ((crs_arr_time::INTEGER)%100) >
-	((crs_dept_time::INTEGER)/100)*60 + ((crs_dept_time::INTEGER)%100)
-then 
-	((crs_arr_time::INTEGER)/100)*60 + ((crs_arr_time::INTEGER)%100) - ((crs_dept_time::INTEGER)/100)*60 - ((crs_dept_time::INTEGER)%100) < 240
-else
-	((crs_arr_time::INTEGER)/100)*60 + ((crs_arr_time::INTEGER)%100) - ((crs_dept_time::INTEGER)/100)*60 - ((crs_dept_time::INTEGER)%100) + 24*60 < 240
-end;
+select distinct A.city_name
+from flight as F join airport as A
+on (F.origin_airport_long_id = A.long_term_id and F.origin_airport_seq_id = A.sequence_id)
+group by A.city_name
+having 
+max(
+	case when 
+		((crs_arr_time::INTEGER)/100)*60 + ((crs_arr_time::INTEGER)%100) >
+		((crs_dept_time::INTEGER)/100)*60 + ((crs_dept_time::INTEGER)%100)
+	then 
+		((crs_arr_time::INTEGER)/100)*60 + ((crs_arr_time::INTEGER)%100) - ((crs_dept_time::INTEGER)/100)*60 - ((crs_dept_time::INTEGER)%100)
+	else
+		((crs_arr_time::INTEGER)/100)*60 + ((crs_arr_time::INTEGER)%100) - ((crs_dept_time::INTEGER)/100)*60 - ((crs_dept_time::INTEGER)%100) + 24*60
+	end
+) < 240;
