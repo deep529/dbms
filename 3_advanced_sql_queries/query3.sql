e3f0ca7641d778d94275815d2af311ecb8e66520
Devansh Agarwal <thedevanshagarwal@gmail.com>
2019-04-05 23:53:18 +0530

    Update query3.sql

--- 3_advanced_sql_queries/query3.sql
+++ 3_advanced_sql_queries/query3.sql
@@ -1,5 +1,10 @@
-select distinct(temp.airline_carrier_code) 
-from (select count((day_of_month, month, airline_carrier_code)) as num_flights , day_of_month, month, airline_carrier_code 
-      from flight
-      group by (day_of_month, month, airline_carrier_code)) temp 
+select distinct(temp.airline_name) 
+from (
+	select count((F.day_of_month, F.month, F.airline_carrier_code)) as num_flights,
+	day_of_month, month, 
+	airline_carrier_code,
+	airline_name
+	from flight as F join airline as A
+	on (F.airline_carrier_code = A.unique_carrier_code)
+	group by (F.day_of_month, F.month, F.airline_carrier_code, A.airline_name)) temp 
 where num_flights > 1000;
